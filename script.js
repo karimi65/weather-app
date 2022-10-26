006a53c692bbcab9b76498206747ab897ed67f72
karimi65 <ali216k@gmail.com>
2022-10-25 19:47:56 -0500

    add fetch request

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+// Add an API key
 const api = {
   key: "41e538b1f7797f8925dd7c6cbb200f24",
-  baseurl: "https://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
+// Setup an event listener on the search box
 const searchbox = document.querySelector(".search-box");
 searchbox.addEventListener("keypress", setQuery);
 
@@ -11,4 +13,17 @@ function setQuery(e) {
     getResults(searchbox.value);
     console.log(searchbox.value);
   }
+}
+
+// fetch request
+function getResults(query) {
+  fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+    .then((weather) => {
+      return weather.json();
+    })
+    .then(displayResults);
+}
+
+function displayResults(weather) {
+  console.log(weather);
 }
